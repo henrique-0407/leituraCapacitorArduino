c304ab95f68bdde07ca275b7f1461593233a6c7a
henrique-0407 <ra54199507825@acaonsfatima.org.br>
2024-08-16 15:58:53 -0300

    ola

--- arduino2.js
+++ arduino2.js
@@ -12,18 +12,25 @@ const client = mqtt.connect(connectUrl, {
   clientId,
   clean: true,
   connectTimeout: 4000,
-  username: 'gustavo.oliveira07022000@gmail.com',
-  password: 'Simone123',
+  username: '',
+  password: '',
   reconnectPeriod: 1000,
 })
 const topic = 'gustavo.oliveira07022000@gmail.com/T1'
 
-const port = new SerialPort({ path: 'COM4', baudRate: 115200});
+const port = new SerialPort({ path: 'COM3', baudRate: 115200});
 const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }))
 
 let receivedData = []; // Variável para armazenar os dados recebidos
 
-
+client.on('connect', () => {
+  client.subscribe(topic, (err) => {
+    if (!err) {
+      console.log(`${topic}`);
+    }
+    
+  });
+});
 // Registre um ouvinte de evento para os dados recebidos
 parser.on('data', (data) => {
   receivedData = data; // Concatene os dados recebidos na variável
@@ -34,14 +41,7 @@ parser.on('data', (data) => {
   });
 });
 
-client.on('connect', () => {
-  client.subscribe(topic, (err) => {
-    if (!err) {
-      console.log(`${topic}`);
-    }
-    
-  });
-});
+
 
 client.on('message', (topic, message) => {
   // Quando uma mensagem for recebida, envie-a para o Arduino
